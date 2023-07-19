ba3a6cf7e4330f7f21ecd7db5d7e1ec14ae1099d
Evgeniy Sokolov <Evgeniy.Sokolov@solarisbank.de>
2023-07-19 10:26:23 +0300

    Updated

--- run-db.bat
+++ run-db.bat
@@ -1,6 +1,1 @@
-docker run -d ^
-  -e POSTGRES_USER=demo ^
-  -e POSTGRES_PASSWORD=demo ^
-  -p 5432:5432 ^
-  -v postgres-data:/var/lib/postgresql/data ^
-  postgres:15.2
+docker run -d -e POSTGRES_USER=demo -e POSTGRES_PASSWORD=demo -p 5432:5432  -v postgres-data:/var/lib/postgresql/data postgres:15.2
